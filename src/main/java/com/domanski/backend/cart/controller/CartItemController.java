e9a437b7d545346b5abf7da7451770bdee3f730a
Dominik <dono3108@gmail.com>
2022-11-28 18:18:00 +0100

    add cart icon counter

--- src/main/java/com/domanski/backend/cart/controller/CartItemController.java
+++ src/main/java/com/domanski/backend/cart/controller/CartItemController.java
@@ -2,10 +2,7 @@ package com.domanski.backend.cart.controller;
 
 import com.domanski.backend.cart.service.CartItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cartItems")
@@ -17,5 +14,10 @@ public class CartItemController {
     @DeleteMapping("/{id}")
     public void deleteCartItem(@PathVariable Long id) {
         cartItemService.deleteCartItem(id);
+    }
+
+    @GetMapping("/count/{cartId}")
+    public Long countCartItems(@PathVariable Long cartId) {
+        return cartItemService.countItemsInCart(cartId);
     }
 }
