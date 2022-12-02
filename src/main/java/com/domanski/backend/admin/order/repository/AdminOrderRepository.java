ab8e7603123504bb1d455684a30cd8c3919b7495
Dominik <dono3108@gmail.com>
2022-12-03 00:52:28 +0100

    create order export function

--- src/main/java/com/domanski/backend/admin/order/repository/AdminOrderRepository.java
+++ src/main/java/com/domanski/backend/admin/order/repository/AdminOrderRepository.java
@@ -1,7 +1,12 @@
 package com.domanski.backend.admin.order.repository;
 
 import com.domanski.backend.admin.order.model.AdminOrder;
+import com.domanski.backend.admin.order.model.AdminOrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AdminOrderRepository extends JpaRepository<AdminOrder, Long> {
+    List<AdminOrder> findAllByPlaceDateIsBetweenAndOrderStatus(LocalDateTime from, LocalDateTime to, AdminOrderStatus orderStatus);
 }
---
+++ src/main/java/com/domanski/backend/admin/order/service/AdminExportService.java
@@ -0,0 +1,22 @@
+package com.domanski.backend.admin.order.service;
+
+import com.domanski.backend.admin.order.model.AdminOrder;
+import com.domanski.backend.admin.order.model.AdminOrderStatus;
+import com.domanski.backend.admin.order.repository.AdminOrderRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminExportService {
+
+    private final AdminOrderRepository adminOrderRepository;
+
+    public List<AdminOrder> exportOrders(LocalDateTime from, LocalDateTime to, AdminOrderStatus orderStatus) {
+        return adminOrderRepository.findAllByPlaceDateIsBetweenAndOrderStatus(from, to, orderStatus);
+    }
+}
