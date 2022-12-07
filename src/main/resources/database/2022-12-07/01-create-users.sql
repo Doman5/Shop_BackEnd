18e8d499f2c2563afc625af8070574b6567d0fac
Dominik <dono3108@gmail.com>
2022-12-07 14:59:13 +0100

    configure security for admin pages

---
+++ src/main/resources/database/2022-12-07/01-create-users.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+--changeset ddomanski:18
+create table users(
+                      id bigint not null auto_increment PRIMARY KEY,
+                      username varchar(50) not null unique,
+                      password varchar(500) not null,
+                      enabled boolean not null
+);
+
+--changeset mdabrowski:19
+create table authorities (
+                             username varchar(50) not null,
+                             authority varchar(50) not null,
+                             constraint fk_authorities_users foreign key(username) references users(username)
+);
+
+--changeset mdabrowski:20
+create unique index ix_auth_username on authorities (username,authority);
+
+--changeset mdabrowski:21
+insert into users (id, username, password, enabled)
+values (1, 'admin', '{bcrypt}$2a$10$upzXFsFUOClFRR69OMKF8eajGMRs0vhcSHqvNDKy9yfW45w7o9z6O', true);
+insert into authorities (username, authority) values ('admin','ROLE_ADMIN');
