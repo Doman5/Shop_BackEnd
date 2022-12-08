a09df5f5eac403a25136bd51d649047e3e905df1
Dominik <dono3108@gmail.com>
2022-12-08 19:32:09 +0100

    add login and registration functions

--- src/main/java/com/domanski/backend/security/LoginController.java
+++ src/main/java/com/domanski/backend/security/LoginController.java
@@ -2,60 +2,108 @@ package com.domanski.backend.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.domanski.backend.security.model.ShopUserDetails;
+import com.domanski.backend.security.model.User;
+import com.domanski.backend.security.model.UserRole;
+import com.domanski.backend.security.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class LoginController {
 
     private final AuthenticationManager authenticationManager;
 
-    private long expirationTime;
-    private String secret;
+    private final long expirationTime;
+    private final String secret;
+    private final UserRepository userRepository;
 
     public LoginController(AuthenticationManager authenticationManager,
                            @Value("${jwt.expirationTime}") long expirationTime,
-                           @Value("${jwt.secret}") String secret) {
+                           @Value("${jwt.secret}") String secret, UserRepository userRepository) {
         this.authenticationManager = authenticationManager;
         this.expirationTime = expirationTime;
         this.secret = secret;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/login")
     public Token login(@RequestBody LoginCredentials loginCredentials ) {
+        return authenticate(loginCredentials.username, loginCredentials.password);
+    }
+
+    @PostMapping("/register")
+    public Token register(@RequestBody @Valid RegisterCredentials registerCredentials) {
+        if(!registerCredentials.password.equals(registerCredentials.repeatPassword)) {
+            throw new IllegalArgumentException("Hasła nie są identyczne");
+        }
+        if(userRepository.existsByUsername(registerCredentials.username)) {
+            throw new IllegalArgumentException("Taki użytkownika już istnieje w bazie");
+        }
+        userRepository.save(User.builder()
+                        .username(registerCredentials.username)
+                        .password("{bcrypt}" + new BCryptPasswordEncoder().encode(registerCredentials.password))
+                        .enabled(true)
+                        .authorities(List.of(UserRole.ROLE_CUSTOMER))
+                        .build());
+                return authenticate(registerCredentials.username, registerCredentials.password);
+    }
+
+    private Token authenticate(String username, String password) {
+
         Authentication authenticate = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginCredentials.username, loginCredentials.password)
+                new UsernamePasswordAuthenticationToken(username, password)
         );
-
-        UserDetails principal = (UserDetails) authenticate.getPrincipal();
-
+        ShopUserDetails principal = (ShopUserDetails) authenticate.getPrincipal();
         String token = JWT.create()
-                .withSubject(principal.getUsername())
+                .withSubject(String.valueOf(principal.getId()))
                 .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
                 .sign(Algorithm.HMAC256(secret));
+        return new Token(token, principal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter(s -> UserRole.ROLE_ADMIN.name().equals(s))
+                .map(s -> true)
+                .findFirst()
+                .orElse(false));
 
-        return new Token(token);
     }
 
     @Getter
     private static class LoginCredentials {
+
         private String username;
         private String password;
+    }
+
+    @Getter
+    private static class RegisterCredentials {
+        @Email
+        private String username;
+        @NotBlank
+        private String password;
+        @NotBlank
+        private String repeatPassword;
     }
 
     @Getter
     @AllArgsConstructor
     private static class Token {
         private String token;
+        private boolean adminAccess;
     }
 }
--- src/main/java/com/domanski/backend/security/SecurityConfig.java
+++ src/main/java/com/domanski/backend/security/SecurityConfig.java
@@ -1,22 +1,19 @@
 package com.domanski.backend.security;
 
+import com.domanski.backend.security.model.UserRole;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-
-
 @Configuration
-@EnableWebSecurity(debug = true)
+//@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     private String secret;
@@ -31,7 +28,7 @@ public class SecurityConfig {
                                            UserDetailsService userDetailsService
     ) throws Exception {
         http.authorizeRequests(authorize -> authorize
-                .antMatchers("/admin/**").authenticated()
+                .antMatchers("/admin/**").hasRole(UserRole.ROLE_ADMIN.getRole())
                 .anyRequest().permitAll()
         );
         http.csrf().disable();
@@ -43,10 +40,5 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
     }
 }
